b0bfd1ad128c197ed22ded0bee180d90d4294317
FoolProof <islamsakrak2002@gmail.com>
2022-05-31 18:45:30 +0200

    Separating the configurations from file management

---
+++ src/main/java/org/foolProof/PasswordVault/File/FileManagement.java
@@ -0,0 +1,29 @@
+package org.foolProof.PasswordVault.File;
+
+import org.foolProof.PasswordVault.User.ClientConfig;
+
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class FileManagement {
+    private final ArrayList<String> emails = new ArrayList<>();
+
+    public ArrayList<String> getEmails() {
+        return emails;
+    }
+
+    private void addAllEmails() {
+        Path path = Paths.get("src/main/resources/mockData/userEmails.csv");
+        try (Stream<String> stream = Files.lines(path)) {
+            stream.forEach(emails::add);
+        } catch (IOException e) {
+            throw new FileSystemNotFoundException("The file could not be found, check the file or adjust the location");
+        }
+    }
+
+}
