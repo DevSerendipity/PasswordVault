881d2c12b35cf46f9d373c3f8997c65e45b9cb3f
FoolProof <islamsakrak2002@gmail.com>
2022-05-31 17:56:44 +0200

    Adding columns for database and improving to string

--- src/main/java/org/foolProof/PasswordVault/User/Client.java
+++ src/main/java/org/foolProof/PasswordVault/User/Client.java
@@ -1,7 +1,8 @@
 package org.foolProof.PasswordVault.User;
 
 import jakarta.persistence.*;
-@Entity
+
+@Entity(name="Client")
 @Table(name = "client")
 public class Client {
     @Id
@@ -15,40 +16,56 @@ public class Client {
             generator = "client_sequence"
     )
 
+    @Column(name = "id")
     private Long id;
+    @Column(name ="email")
     private String email;
+    @Column(name ="file_name")
     private String fileName;
+    @Column(name ="password")
     private String password;
 
-    public Client(String email, String fileName,String password) {
+    public Client(String email, String fileName, String password) {
         this.email = email;
         this.fileName = fileName;
         this.password = password;
     }
-    public Client(){
+
+    public Client() {
 
     }
+
     public String getPassword() {
         return password;
     }
+
     public String getFileName() {
         return fileName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", email='" + email + '\''+
+                ", fileName='" + fileName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
