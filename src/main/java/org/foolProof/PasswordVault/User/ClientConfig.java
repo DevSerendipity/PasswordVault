58eed928bd76bf35b9ec98c6a7077ec013c4b81b
FoolProof <islamsakrak2002@gmail.com>
2022-05-31 17:57:36 +0200

    Retrieving clients from a csv files and publishing it

--- src/main/java/org/foolProof/PasswordVault/User/ClientConfig.java
+++ src/main/java/org/foolProof/PasswordVault/User/ClientConfig.java
@@ -5,27 +5,42 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Configuration
 public class ClientConfig {
+    private final ArrayList<String> emails = new ArrayList<>();
 
     @Bean
-    CommandLineRunner commandLineRunner(ClientRepository clientRepository){
+    CommandLineRunner commandLineRunner(ClientRepository clientRepository) {
+        int getLastClientById = clientRepository.findClientById();
+
+        addAllEmails();
+        System.out.println("START");
         return args -> {
             Encryption encryption = new Encryption();
-            Client firstClient = new Client(
-                    "person@gmail.com",
-                    "Notion",
-                    encryption.generateHashPassword("firstClient")
-
-            );
-            Client secondClient = new Client(
-                    "barcode@gmail.com",
-                    "Obsidian",
-                    encryption.generateHashPassword("secondClient")
-            );
-        clientRepository.saveAll(List.of(firstClient,secondClient));
+            for (int i = 1; i < 2; i++) {
+                clientRepository.saveAll(List.of
+                        (new Client(emails.get(getLastClientById + i),
+                                "Notion",
+                                encryption.generateHashPassword(emails.get((getLastClientById + i))))));
+            }
         };
+    }
+
+    private void addAllEmails() {
+        Path path = Paths.get("src/main/resources/mockData/userEmails.csv");
+        try (Stream<String> stream = Files.lines(path)) {
+            stream.forEach(emails::add);
+        } catch (IOException e) {
+            throw new FileSystemNotFoundException("The file could not be found, check the file or adjust the location");
+        }
     }
 }
