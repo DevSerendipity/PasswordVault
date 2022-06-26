089439fa431c38051900a06288b4b33c36a50879
FoolProof <islamsakrak2002@gmail.com>
2022-06-26 16:04:25 +0200

    Adding Client registration, login, creating new clients and redirection

--- src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
+++ src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
@@ -1,22 +1,52 @@
 package org.foolProof.PasswordVault.web;
 
+import org.foolProof.PasswordVault.User.Client;
+import org.foolProof.PasswordVault.User.ClientService;
+import org.foolProof.PasswordVault.encryption.Encryption;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class WebsiteController {
+import java.util.List;
 
-    @GetMapping("/register")
-    public String getRegistration() {
+@Controller public class WebsiteController {
+
+    private final ClientService clientService;
+
+    public WebsiteController( ClientService clientService ) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/register") public String register() {
         return "/register.html";
     }
 
-    @GetMapping("/getFolder")
-    public String getFolderSelected() {
+    @GetMapping("/login") public String getRegistration() {
+        return "/login.html";
+    }
+
+    @GetMapping("/getFolder") public String getFolderSelected() {
         return "/folderPicker.html";
     }
-    @GetMapping("/password")
-    public String password() {
-        return "/password.html";
+
+    @GetMapping("/getFile") public String getFileSelected() {
+        return "/filePicker.html";
+    }
+
+    @RequestMapping(value = "/password", method = RequestMethod.GET) public String getClient( Model model ) {
+        List<Client> clients = clientService.getAllClients();
+        model.addAttribute( "clients", clients );
+        model.addAttribute( "client", new Client() );
+        return "password";
+    }
+
+    @RequestMapping(value = "/password", method = RequestMethod.POST)
+    public String saveStudent( Model model, @ModelAttribute Client client ) {
+        Encryption encryption = new Encryption();
+        client.setPassword( encryption.generateHashPassword( client.getPassword() ) );
+        System.out.println( client.getPassword() );
+        model.addAttribute( "password", client.getPassword() );
+        clientService.addNewClient( client );
+        return "redirect:/password";
     }
 }
