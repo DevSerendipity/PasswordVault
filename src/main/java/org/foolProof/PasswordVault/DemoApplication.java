fde415cee25f246cef6425ecfd79b3b8d72e6ea0
FoolProof <islamsakrak2002@gmail.com>
2022-07-04 16:06:34 +0200

    Hiding utility from class constructors

--- src/main/java/org/foolProof/PasswordVault/DemoApplication.java
+++ src/main/java/org/foolProof/PasswordVault/DemoApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication @RestController public class DemoApplication {
+
+    private DemoApplication() {
+        
+    }
+
     public static void main( String[] args ) {
         SpringApplication.run( DemoApplication.class, args );
     }
