910c655dad964bb71db9bee24d330a8a480590c9
FoolProof <islamsakrak2002@gmail.com>
2022-07-04 16:17:23 +0200

    Removing private constructors because it makes the app not work
    It can not start the application without it

--- src/main/java/org/foolProof/PasswordVault/DemoApplication.java
+++ src/main/java/org/foolProof/PasswordVault/DemoApplication.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication @RestController public class DemoApplication {
 
-    private DemoApplication() {
-        
-    }
-
     public static void main( String[] args ) {
         SpringApplication.run( DemoApplication.class, args );
     }
