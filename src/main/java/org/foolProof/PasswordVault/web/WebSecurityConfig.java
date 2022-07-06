863d4f093bec9a788d5cb95534f4a4caadd7c376
FoolProof <islamsakrak2002@gmail.com>
2022-07-06 15:44:14 +0200

    Removing un used imports and formating

--- src/main/java/org/foolProof/PasswordVault/web/WebSecurityConfig.java
+++ src/main/java/org/foolProof/PasswordVault/web/WebSecurityConfig.java
@@ -7,17 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration @EnableWebSecurity public class WebSecurityConfig {
 
@@ -34,13 +26,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
         System.out.println( passwordEncoder.encode( "password" ) );
         String password = passwordEncoder.encode( "password" );
 
-        UserDetails user = User.withUsername( "user" )
-                .password( "{argon2}"+password ).roles( "USER" )
-                .build();
+        UserDetails user = User.withUsername( "user" ).password( "{argon2}" + password ).roles( "USER" ).build();
         return new InMemoryUserDetailsManager( user );
     }
 
-    public static PasswordEncoder argon2(){
+    public static PasswordEncoder argon2() {
         return new Argon2PasswordEncoder();
     }
 
