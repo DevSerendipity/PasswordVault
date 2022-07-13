767175d2b3111009eb823fdbe8f2b1fcc08b436e
FoolProof <58050604+IslamSakrak@users.noreply.github.com>
2022-07-13 14:48:42 +0200

    Changing the password encoding from PBKDF to Argon2 (#8)
    * Changing the password encoding from PBKDF to Argon2
    
    * Under 120 lines and improving the spacing
    
    * adding whitespace

--- src/main/java/org/foolProof/PasswordVault/User/ClientConfig.java
+++ src/main/java/org/foolProof/PasswordVault/User/ClientConfig.java
@@ -1,28 +1,27 @@
 package org.foolProof.PasswordVault.User;
 
 import org.foolProof.PasswordVault.File.FileManagement;
-import org.foolProof.PasswordVault.cryptography.PasswordEncryption;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 import java.util.List;
 
 @Configuration public class ClientConfig {
     private final FileManagement fileManagement = new FileManagement();
 
-    @Bean CommandLineRunner commandLineRunner( ClientRepository clientRepository ) {
+    @Bean CommandLineRunner commandLineRunner(ClientRepository clientRepository) {
         int getLastClientById = clientRepository.findClientById();
         fileManagement.addAllEmails();
-        System.out.println( "START" );
+        System.out.println("START");
         return args -> {
-            PasswordEncryption passwordEncryption = new PasswordEncryption();
+            Argon2PasswordEncoder argon2PasswordEncoder = new Argon2PasswordEncoder();
             for ( int i = 0; i < 2; i++ ) {
-                clientRepository.saveAll( List.of( new Client( fileManagement.getEmails().get( getLastClientById + i ),
-                                                               passwordEncryption.generateHashPassword(
-                                                                       fileManagement.getEmails()
-                                                                               .get( ( getLastClientById
-                                                                                       + i ) ) ) ) ) );
+                clientRepository.saveAll(List.of(new Client(fileManagement.getEmails().get(getLastClientById + i),
+                                                            argon2PasswordEncoder.encode(fileManagement.getEmails()
+                                                                                                 .get((getLastClientById
+                                                                                                         + i))))));
             }
         };
     }
