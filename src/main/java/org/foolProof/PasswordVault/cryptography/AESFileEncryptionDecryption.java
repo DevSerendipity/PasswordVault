9c802fa0b74ea29e5801e3161e04bf28923078fb
FoolProof <58050604+IslamSakrak@users.noreply.github.com>
2022-07-13 16:28:37 +0200

    Refactoring the file encryption and decryption (#10)
    * Refactoring the file encryption and decryption
    
    Removing all the places where exceptions are passed down to other classes and extracted them and turned them to methods instead which are also surrounded inside within try catch blocks
    
    * Improving bit of spacing
    
    * adding white space to catch blocks

--- src/main/java/org/foolProof/PasswordVault/cryptography/AESFileEncryptionDecryption.java
+++ src/main/java/org/foolProof/PasswordVault/cryptography/AESFileEncryptionDecryption.java
@@ -23,86 +23,147 @@ public class AESFileEncryptionDecryption {
     private static final int ITERATION_COUNT = 65536;
     private static final int KEY_LENGTH = 256;
 
-    public static SecretKey getAESKeyFromPassword( char[] password, byte[] salt )
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        SecretKeyFactory factory = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA256" );
-        KeySpec spec = new PBEKeySpec( password, salt, ITERATION_COUNT, KEY_LENGTH );
-        return new SecretKeySpec( factory.generateSecret( spec ).getEncoded(), "AES" );
+    public static SecretKey getAESKeyFromPassword(char[] password, byte[] salt) {
+        KeySpec spec = new PBEKeySpec(password, salt, ITERATION_COUNT, KEY_LENGTH);
+        try {
+            return new SecretKeySpec(getSecretKey().generateSecret(spec).getEncoded(), "AES");
+        } catch ( InvalidKeySpecException e ) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static byte[] getRandomNonce( int numBytes ) {
-        byte[] nonce = new byte[ numBytes ];
-        new SecureRandom().nextBytes( nonce );
+    public static SecretKeyFactory getSecretKey() {
+        SecretKeyFactory factory;
+        try {
+            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        } catch ( NoSuchAlgorithmException e ) {
+            throw new RuntimeException(e);
+        }
+        return factory;
+    }
+
+
+    public static byte[] getRandomNonce(int numBytes) {
+        byte[] nonce = new byte[numBytes];
+        new SecureRandom().nextBytes(nonce);
         return nonce;
     }
 
-    public static byte[] encrypt( byte[] pText, String password )
-            throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException,
-            IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException {
-        byte[] salt = getRandomNonce( SALT_LENGTH_BYTE );
+    public static byte[] encrypt(byte[] pText, String password) {
+        byte[] salt = getRandomNonce(SALT_LENGTH_BYTE);
         // GCM recommended 12 bytes iv?
-        byte[] iv = getRandomNonce( IV_LENGTH_BYTE );
-        SecretKey aesKeyFromPassword = getAESKeyFromPassword( password.toCharArray(), salt );
-        Cipher cipher = Cipher.getInstance( ENCRYPT_ALGO );
+        byte[] iv = getRandomNonce(IV_LENGTH_BYTE);
+        SecretKey aesKeyFromPassword = getAESKeyFromPassword(password.toCharArray(), salt);
+        Cipher cipher = getCipherInstance();
         // ASE-GCM needs GCMParameterSpec
-        cipher.init( Cipher.ENCRYPT_MODE, aesKeyFromPassword, new GCMParameterSpec( TAG_LENGTH_BIT, iv ) );
-        byte[] cipherText = cipher.doFinal( pText );
-        return ByteBuffer.allocate( iv.length + salt.length + cipherText.length ).put( iv ).put( salt )
-                .put( cipherText ).array();
+        cipherInitialization(cipher, Cipher.ENCRYPT_MODE, aesKeyFromPassword, iv);
+        byte[] cipherText = getCipherText(pText, cipher);
+        return ByteBuffer.allocate(iv.length + salt.length + cipherText.length).put(iv).put(salt).put(cipherText)
+                .array();
+    }
+
+    private static void cipherInitialization(Cipher cipher, int encryptMode, SecretKey aesKeyFromPassword, byte[] iv) {
+        try {
+            cipher.init(encryptMode, aesKeyFromPassword, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        } catch ( InvalidKeyException | InvalidAlgorithmParameterException e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] getCipherText(byte[] pText, Cipher cipher) {
+        byte[] cipherText;
+        try {
+            cipherText = cipher.doFinal(pText);
+        } catch ( IllegalBlockSizeException | BadPaddingException e ) {
+            throw new RuntimeException(e);
+        }
+        return cipherText;
+    }
+
+    private static Cipher getCipherInstance() {
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(ENCRYPT_ALGO);
+        } catch ( NoSuchAlgorithmException | NoSuchPaddingException e ) {
+            throw new RuntimeException(e);
+        }
+        return cipher;
     }
 
     // we need the same password, salt and iv to decrypt it
-    private static byte[] decrypt( byte[] cText, String password )
-            throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException,
-            InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        ByteBuffer bb = ByteBuffer.wrap( cText );
-        byte[] iv = new byte[ 12 ];
-        bb.get( iv );
+    private static byte[] decrypt(byte[] cText, String password) {
+        ByteBuffer bb = ByteBuffer.wrap(cText);
+        byte[] iv = new byte[12];
+        bb.get(iv);
 
-        byte[] salt = new byte[ 16 ];
-        bb.get( salt );
+        byte[] salt = new byte[16];
+        bb.get(salt);
 
-        byte[] cipherText = new byte[ bb.remaining() ];
-        bb.get( cipherText );
+        byte[] cipherText = new byte[bb.remaining()];
+        bb.get(cipherText);
         // get back the aes key from the same password and salt
-        SecretKey aesKeyFromPassword = getAESKeyFromPassword( password.toCharArray(), salt );
-        Cipher cipher = Cipher.getInstance( ENCRYPT_ALGO );
-        cipher.init( Cipher.DECRYPT_MODE, aesKeyFromPassword, new GCMParameterSpec( TAG_LENGTH_BIT, iv ) );
-        return cipher.doFinal( cipherText );
+        SecretKey aesKeyFromPassword = getAESKeyFromPassword(password.toCharArray(), salt);
+        Cipher cipher = getCipherInstance();
+        cipherInitialization(cipher, Cipher.DECRYPT_MODE, aesKeyFromPassword, iv);
+        try {
+            return cipher.doFinal(cipherText);
+        } catch ( IllegalBlockSizeException | BadPaddingException e ) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void encryptFile( String fromFile, String toFile, String password )
-            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
-            NoSuchAlgorithmException, InvalidKeySpecException, BadPaddingException, InvalidKeyException, IOException,
-            URISyntaxException {
+    public static void encryptFile(String fromFile, String toFile, String password) {
         // read a normal txt file
-        byte[] fileContent = Files.readAllBytes( Paths.get( ClassLoader.getSystemResource( fromFile ).toURI() ) );
+        byte[] fileContent = getFileContent(fromFile);
         // encrypt with a password
-        byte[] encryptedText = encrypt( fileContent, password );
+        byte[] encryptedText = encrypt(fileContent, password);
         // save a file
-        Path path = Paths.get( toFile );
-        Files.write( path, encryptedText );
+        Path path = Paths.get(toFile);
+        writeToFile(encryptedText, path);
     }
 
-    public static byte[] decryptFile( String fromEncryptedFile, String password )
-            throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException
-            , NoSuchAlgorithmException, InvalidKeySpecException, BadPaddingException, InvalidKeyException {
-        byte[] file = Files.readAllBytes( Paths.get( fromEncryptedFile ) );
-        return decrypt( file, password );
+    private static byte[] getFileContent(String fromFile) {
+        byte[] fileContent;
+        try {
+            fileContent = Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(fromFile).toURI()));
+        } catch ( IOException | URISyntaxException e ) {
+            throw new RuntimeException(e);
+        }
+        return fileContent;
     }
 
-    public static void main( String[] args )
-            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
-            NoSuchAlgorithmException, InvalidKeySpecException, BadPaddingException, IOException, InvalidKeyException,
-            URISyntaxException {
+    private static void writeToFile(byte[] encryptedText, Path path) {
+        try {
+            Files.write(path, encryptedText);
+        } catch ( IOException e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] decryptFile(String fromEncryptedFile, String password) {
+        byte[] file = getFile(fromEncryptedFile);
+        return decrypt(file, password);
+    }
+
+    private static byte[] getFile(String fromEncryptedFile) {
+        byte[] file;
+        try {
+            file = Files.readAllBytes(Paths.get(fromEncryptedFile));
+        } catch ( IOException e ) {
+            throw new RuntimeException(e);
+        }
+        return file;
+    }
+
+    public static void main(String[] args) {
         String password = "password123"; // TODO now we need to encrypt this
         String fromFile = "some.txt";
-        String toFile = "C:\\Users\\X\\Desktop\\someText-encrypted.txt";
+        String toFile = "C:\\Users\\Emir\\Desktop\\someText-encrypted.txt";
         // encrypt file
-        encryptFile( fromFile, toFile, password );
+        encryptFile(fromFile, toFile, password);
         // decrypt file
-        byte[] decryptedText = decryptFile( toFile, password );
-        String pText = new String( decryptedText );
-        System.out.println( pText );
+        byte[] decryptedText = decryptFile(toFile, password);
+        String pText = new String(decryptedText);
+        System.out.println(pText);
     }
 }
