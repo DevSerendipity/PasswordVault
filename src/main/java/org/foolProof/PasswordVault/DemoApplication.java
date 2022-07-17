a20daee23f2e2bfba96b1284068fa5a97e67564e
FoolProof <islamsakrak2002@gmail.com>
2022-07-17 15:57:29 +0200

    Adding the feature to being able to upload a file from users computer

--- src/main/java/org/foolProof/PasswordVault/DemoApplication.java
+++ src/main/java/org/foolProof/PasswordVault/DemoApplication.java
@@ -2,11 +2,19 @@ package org.foolProof.PasswordVault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication @RestController public class DemoApplication {
+@SpringBootApplication @RestController @ServletComponentScan public class DemoApplication {
 
+    // todo implement one of this sadly haven't found a way to get the parent directory of a selected file
+    //  only way I think this would work is that webkitdirectory way and tell the user to make an specific folder for it
+    //or two make the user select a directory, do nothing but store the relative path than user selects the file
     public static void main( String[] args ) {
         SpringApplication.run( DemoApplication.class, args );
     }
+    // if i chose to select a directory and encrypt everything in it, than i would need to work on the ability to
+    // being able to send multiple files and than multiple decryption
+    // making it seperate to select a directory than to select a file could result in user selecting 2 unrelated
+    // things which would result in file missing
 }
--- src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
+++ src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
@@ -2,11 +2,18 @@ package org.foolProof.PasswordVault.web;
 
 import org.foolProof.PasswordVault.User.Client;
 import org.foolProof.PasswordVault.User.ClientService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Controller public class WebsiteController {
@@ -17,8 +24,24 @@ import java.util.List;
         this.clientService = clientService;
     }
 
-    @GetMapping("/getFile") public String getFolderSelected() {
-        return "/filePicker.html";
+    @GetMapping("/getFolderSelected") public String getFolderSelected() {
+        return "/folderPicker.html";
+    }
+
+    @Value("${file.upload-dir}")
+    String File_Directory;
+
+    @RequestMapping(value="/uploadFile", method = RequestMethod.POST)
+    public ResponseEntity<Object> fileUpload(@RequestParam("File") MultipartFile file ) throws IOException {
+        File myFile = new File(File_Directory + file.getOriginalFilename());
+
+        myFile.createNewFile();
+
+        FileOutputStream fos = new FileOutputStream(myFile);
+
+        fos.write(file.getBytes());
+        fos.close();
+        return new ResponseEntity<>("The file uploaded successfully", HttpStatus.OK);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET) public String getClient( Model model ) {
