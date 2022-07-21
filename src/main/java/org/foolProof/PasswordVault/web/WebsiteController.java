402fee32c26f0160683c54e4dcf2b3f26d7f3a6f
FoolProof <58050604+IslamSakrak@users.noreply.github.com>
2022-07-21 11:04:21 +0200

    adding the file encryption upon file selection (#14)
    * adding the file encryption upon file selection
    
    * moving the script tags and deleting unused imports
    
    * Removing unnecessary page and js

--- src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
+++ src/main/java/org/foolProof/PasswordVault/web/WebsiteController.java
@@ -1,9 +1,14 @@
 package org.foolProof.PasswordVault.web;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.foolProof.PasswordVault.User.Client;
 import org.foolProof.PasswordVault.User.ClientService;
+import org.foolProof.PasswordVault.cryptography.AESFileEncryptionDecryption;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -14,6 +19,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller public class WebsiteController {
@@ -25,19 +33,34 @@ import java.util.List;
         this.clientService = clientService;
     }
 
-    @GetMapping("/getFolderSelected") public String getFolderSelected() {
-        return "/folderPicker.html";
+    @GetMapping("/file") public String file() {
+        return "/filePicker.html";
     }
 
-
     @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    public ResponseEntity<Object> fileUpload(@RequestParam("File") MultipartFile file) throws IOException {
-        File myFile = new File(fileDirectory + file.getOriginalFilename());
+    public void fileUpload(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws IOException {
+        String fileName = file.getOriginalFilename();
+        File myFile = new File(fileDirectory + fileName);
         myFile.createNewFile();
         FileOutputStream fos = new FileOutputStream(myFile);
         fos.write(file.getBytes());
         fos.close();
-        return new ResponseEntity<>("The file uploaded successfully", HttpStatus.OK);
+        AESFileEncryptionDecryption.startCryptography(fileDirectory + fileName);
+        response.sendRedirect("/download/" + fileName + "-encrypted.txt");
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Object> downloadFileFromLocal(@PathVariable String fileName) {
+        Path path = Paths.get(fileDirectory + fileName);
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch ( MalformedURLException e ) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/pdf"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET) public String getClient(Model model) {
