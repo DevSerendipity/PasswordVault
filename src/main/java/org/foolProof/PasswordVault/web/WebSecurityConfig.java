e1d1ad20566c8b0a2d1cecb44e158233af48b5cb
Serendipity <58050604+IslamSakrak@users.noreply.github.com>
2022-07-27 16:37:15 +0200

    Making the auth more neat and smaller (#91)
    * Making the auth more neat and smaller
    
    * changing the modifiers

--- src/main/java/org/foolProof/PasswordVault/web/WebSecurityConfig.java
+++ src/main/java/org/foolProof/PasswordVault/web/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package org.foolProof.PasswordVault.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,19 +15,27 @@ import org.springframework.security.web.SecurityFilterChain;
 
     @Autowired private MyUserDetailsService myUserDetailsService;
 
-    @Bean public SecurityFilterChain websiteFilter(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/getFile").authenticated().antMatchers("/uploadFile", "/register")
-                .permitAll().and().formLogin().permitAll().and().logout().permitAll();
-        return http.build();
-    }
+    @Autowired private PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        System.out.println("Auth");
-        auth.userDetailsService(myUserDetailsService);
+    @Bean public SecurityFilterChain websiteFilter(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/file").authenticated().antMatchers("/uploadFile", "/register")
+                .permitAll().and().formLogin().defaultSuccessUrl("/file").loginPage("/login")
+                .failureUrl("/login?error=true").permitAll().and().logout().permitAll();
+        return http.build();
     }
 
     @Bean public static PasswordEncoder argon2() {
         return new Argon2PasswordEncoder();
     }
 
+    @Bean public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        authenticationProvider.setUserDetailsService(myUserDetailsService);
+        return authenticationProvider;
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider());
+    }
 }
